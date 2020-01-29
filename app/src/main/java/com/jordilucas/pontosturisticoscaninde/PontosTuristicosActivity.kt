0c6680c5d8d078407c82129ec091073dcf4775ec
Jordi <jordilucas3@gmail.com>
2020-01-29 14:42:52 -0300

    Colors, icon

---
+++ app/src/main/java/com/jordilucas/pontosturisticoscaninde/PontosTuristicosActivity.kt
@@ -0,0 +1,12 @@
+package com.jordilucas.pontosturisticoscaninde
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PontosTuristicosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pontos_turisticos)
+    }
+}
