03a57bb03a73e0cbeb5a59026fec7c95c11be384
Jordi <jordilucas3@gmail.com>
2020-01-29 13:08:34 -0300

    Tela de descrição dos pontos turisticos

--- app/src/main/java/com/jordilucas/pontosturisticoscaninde/DescriptionActivity.java
+++ app/src/main/java/com/jordilucas/pontosturisticoscaninde/DescriptionActivity.java
@@ -5,14 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity {
+public class DescriptionActivity extends AppCompatActivity {
 
     ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.descricao);
+        setContentView(R.layout.activity_description);
         imageView = findViewById(R.id.imgView);
         Picasso.get().load("http://blogs.opovo.com.br/c4noticias/wp-content/uploads/sites/15/2015/06/M1Q4539.jpg").into(imageView);
     }
