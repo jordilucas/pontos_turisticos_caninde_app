16ac2e765d6f0b87c01da32ad7483afb2130756e
Jordi <jordilucas3@gmail.com>
2020-02-02 10:09:54 -0300

    Create picasso extensions

---
+++ app/src/main/java/com/jordilucas/pontosturisticoscaninde/extensions/Picasso.kt
@@ -0,0 +1,31 @@
+package com.jordilucas.pontosturisticoscaninde.extensions
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import java.lang.Exception
+
+fun ImageView.loadUrl(url: String?, progress: ProgressBar? = null){
+    if(url == null || url.trim().isEmpty()){
+        setImageBitmap(null)
+        return
+    }
+    if(progress == null){
+        Picasso.get().load(url).fit().into(this)
+    }
+    else{
+        progress.visibility = View.VISIBLE
+        Picasso.get().load(url).fit().into(this, object : Callback{
+            override fun onSuccess() {
+                progress.visibility = View.GONE
+            }
+
+            override fun onError(e: Exception?) {
+                progress.visibility = View.GONE
+            }
+
+        })
+    }
+}
